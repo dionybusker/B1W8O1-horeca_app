81c4cee858ca75b5407877b2708f7cce98babf2d
dionybusker <99057959@mydavinci.nl>
2019-11-01 09:02:50 +0100

    function addOrder

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 // script
+
+function showBill() { // de rekening wordt getoond
+    // code
+}
+
+function addOrder() { // bestellingen worden toegevoegd
+    // code
+    var order = "Welke bestelling wilt u toevoegen?";
+    var options = "U kunt kiezen uit fris, bier of wijn.";
+    var quit = "Als u de rekening wilt hebben, vul dan 'stop' in.";
+    var question = prompt(order + "\n" + options + "\n" + quit);
+    if (question == "fris") {
+        prompt("Hoeveel fris wilt u toevoegen aan uw bestelling?");
+    } else if (question == "bier") {
+        prompt("Hoeveel bier wilt u toevoegen aan uw bestelling?");
+    } else if (question == "wijn") {
+        prompt("Hoeveel wijn wilt u toevoegen aan uw bestelling?");
+    } else {
+        alert("Sorry, maar dat hebben wij niet. U kunt alleen kiezen uit fris, bier of wijn.");
+    }
+    addOrder();
+}
+
+function calcPrice() { // prijzen worden uitgerekend
+    // code
+}
+
+addOrder();
